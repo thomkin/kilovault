89f403ae595dc45882161bd9611830f9c5081eea
thomas <thkinder@gmail.com>
2026-03-18 14:25:24 +0530

    added config to force everything to go to the rpc route and not using http route at all

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ const PORT = 5096;
 console.log(`🚀 Server listening on http://localhost:${PORT}`);
 
 BunnySDK.net.http.serve({ port: PORT, hostname: "0.0.0.0" }, (req) => {
-  return handleRequest(req);
+  return handleRequest(req, {
+    rpcOnly: true,
+  });
 });
